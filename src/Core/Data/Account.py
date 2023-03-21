d901ad3b5c2e74577ff57bc93e753808386e6f4d
ChopstickLeg <carpenterhayden1010@gmail.com>
2023-03-21 19:11:52 -0400

    Began DB implementation

---
+++ src/Core/Data/Account.py
@@ -0,0 +1,7 @@
+class Acccount():
+    def __init__(self, user, passwd):
+        self.key = None
+        self.user = user
+        self.passwd = passwd
+        self.gamesPlayed = 0
+        self.gamesWon = 0
